6955066e938b5f5a02baa2970438f35382e6b14a
panda1114Git <panda.kk1114@gmail.com>
2021-12-14 14:03:25 +0000

    used boilerplate

---
+++ src/components/app/app.tsx
@@ -0,0 +1,28 @@
+/**
+ * Created by: Andrey Polyakov (andrey@polyakov.im)
+ */
+
+import cn from 'classnames';
+import React, {Suspense, lazy} from 'react';
+
+import {stylesContainer} from './app.module.less';
+import {stylesHeader, stylesImage, stylesLink} from './app.module.scss';
+
+const LazyStrawberryIcon = lazy(() => import('./strawberry'));
+export const App = (): React.ReactElement => (
+    <div className={stylesContainer}>
+        <div className={stylesHeader}>It works</div>
+        <Suspense fallback={'loading...'}>
+            <LazyStrawberryIcon className={stylesImage} />
+        </Suspense>
+        <div>
+            <a
+                className={cn(stylesLink)}
+                href="https://github.com/glook/webpack-typescript-react"
+                target="_blank"
+            >
+                @glook/webpack-typescript-react
+            </a>
+        </div>
+    </div>
+);
